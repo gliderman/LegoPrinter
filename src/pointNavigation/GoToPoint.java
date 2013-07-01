939acafc12ceb4d07b25177b53a6625b6ff37066
gliderman <Nate@Nates-MacBook-Pro.local>
2013-07-01 15:34:29 -0400

    Math update
    I have not tested this yet, but this is mainly the math code for having
    the arm go to points on the plate.

--- src/pointNavigation/GoToPoint.java
+++ src/pointNavigation/GoToPoint.java
@@ -1,15 +1,33 @@
 package pointNavigation;
 
 import lejos.geom.Point;
+import lejos.nxt.Motor;
 
 public class GoToPoint {
 	
 	Point currentPoint;
 	Point lastPoint;
+	double distToCenterOfPlate = 10.0; // in mm
+	double distToEndOfArm = distToCenterOfPlate;
 
 	public GoToPoint() {
 		currentPoint	= new Point(0, 0);
 		lastPoint		= new Point(0, 0);
+		
+		// Stop motors
+		Motor.A.stop();
+		Motor.B.stop();
+		Motor.C.stop();
+		
+		// deg/sec
+		Motor.A.setSpeed(180);
+		Motor.B.setSpeed(90);
+		Motor.C.setSpeed(720);
+
+		// Reset motors
+		Motor.A.resetTachoCount();
+		Motor.B.resetTachoCount();
+		Motor.C.resetTachoCount();
 	}
 	
 	public Point getCurrentPoint() {
@@ -29,8 +47,85 @@ public class GoToPoint {
 		lastPoint = point;
 	}
 	
-	public void goToPoint(Point point, boolean withLift) {
+	public void goToPointWithPen(Point point, boolean withLift) {
+		if (point == currentPoint) {
+			// Why should it move?
+		} else {
+			// Lift pen
+			if (withLift) {
+				Motor.C.rotateTo(-720);
+			}
+			
+			double[] angles = calculateAnglesForPoint(point);
+			angles[0] = correctAngleForPlate(angles[0], point);
+			
+			Motor.A.rotateTo((int) angles[0], true);
+			Motor.B.rotateTo(-((int) angles[1]), false);
+			
+			// Lower pen
+			if (withLift) {
+				Motor.C.rotateTo(0);
+			}
+		}
+	}
+	
+	/**
+	 * This method returns an array of doubles filled with the
+	 * angles of the triangle created between the center of the
+	 * plate, the arm pivot point, and the actual point to go to.
+	 * And it returns them in that order, too.
+	 * @param p
+	 * @return angles
+	 */
+	private double[] calculateAnglesForPoint(Point p) {
+		double centerToPoint = calculateDistFromCenterToPoint(p);
+		double[] angles = {0.0, 0.0, 0.0};
 		
+		angles[0] = findAngleForTriangleLengths(distToEndOfArm,
+				distToCenterOfPlate, centerToPoint);
+		angles[1] = findAngleForTriangleLengths(centerToPoint,
+				distToCenterOfPlate, distToEndOfArm);
+		angles[2] = findAngleForTriangleLengths(distToCenterOfPlate,
+				centerToPoint, distToEndOfArm);
+		
+		return angles;
+	}
+	
+	private double findAngleForTriangleLengths(double sideOpposite,
+			double otherSide, double anotherSide) {
+		
+		double theOtherSidesMultiplied = (2 * otherSide * anotherSide);
+		double sidesAddedAndSquared = (Math.pow(otherSide, 2) +
+				Math.pow(anotherSide, 2) - Math.pow(sideOpposite, 2));
+		
+		double neededAngle =
+				Math.acos(theOtherSidesMultiplied/sidesAddedAndSquared);
+		
+		return neededAngle;
+	}
+	
+	private double calculateDistFromCenterToPoint(Point p) {
+		double length = Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2));
+		return length;
+	}
+	
+	private double correctAngleForPlate(double originalAngle, Point p) {
+		// If the point is with completely positive coordinates,
+		// then it leaves the angle alone.
+		
+		if ((p.x < 0) && (p.y >= 0)) {
+			originalAngle += 270;
+		}
+		
+		if ((p.x >=0) && (p.y < 0)) {
+			originalAngle += 90;
+		}
+		
+		if ((p.x < 0) && (p.y < 0)) {
+			originalAngle += 180;
+		}
+		
+		return originalAngle;
 	}
 	
 }
