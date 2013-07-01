1024ee1aca34837fcc80f24dbf22e8fd6a0adddc
gliderman <Nate@Nates-MacBook-Pro.local>
2013-07-01 08:07:53 -0400

    Start of Lego_Printer main file
    Just trying to get some basic code written, and also get used to using
    GitHub.

---
+++ src/userInterface/Lego_Printer.java
@@ -0,0 +1,17 @@
+package userInterface;
+
+// "Lego_Printer" is what will be shown on the NXT's file selector
+public class Lego_Printer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Lego_Printer();
+	}
+	
+	public Lego_Printer() {
+		
+	}
+
+}
